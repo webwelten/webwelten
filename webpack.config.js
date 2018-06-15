bdf81c812fa48b58ccddf4e34c19eb718562ad9c
Michael Abt <michael@webwelten.com>
2018-06-15 11:39:13 +0200

    Add css-modules and styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const context = path.resolve(__dirname);
 
 module.exports = {
+  context,
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
@@ -14,7 +18,45 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          query: {
+            cacheDirectory: false,
+            plugins: [
+              'transform-react-jsx',
+              'transform-object-rest-spread',
+              [
+                'react-css-modules',
+                {
+                  context,
+                },
+              ],
+            ],
+          },
         },
+      },
+      {
+        test: /\.(css|scss)$/,
+        include: path.resolve(__dirname, './src'),
+        oneOf: [
+          {
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: {
+                  //sourceMap: true,
+                  modules: true,
+                  importLoaders: 2,
+                  //localIdentName: '[folder]--[local]--[hash:base64:2]',
+                  localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+                },
+              },
+            ],
+            exclude: /style/,
+          },
+          {
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
       },
       {
         test: /\.html$/,
@@ -24,6 +66,10 @@ module.exports = {
             options: { minimize: true },
           },
         ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
       },
     ],
   },
