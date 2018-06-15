c1ef1ee5144d9e2d08d97edee276c12c7eb325f5
Michael Abt <michael@webwelten.com>
2018-06-15 11:47:07 +0200

    Add Intro pattern/container

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom';
 
 import './style/main.css';
 
+import Intro from './patterns/Intro';
+
 import Button from './components/Button';
 import Section from './components/Section';
 
 const App = () => (
   <div className="container">
+    <Intro />
+
     <Section shadow>
       <p>Hello World!</p>
-      <p><a href="/test.html">Das ist ein Link…</a></p>
+      <p>
+        <a href="/test.html">Das ist ein Link…</a>
+      </p>
       <Button href="/neu.html">Test</Button>
       <ul className="list-inline">
         <li>Test 1</li>
         <li>Test 2</li>
         <li>Test 3</li>
       </ul>
-      <footer className="footer">
-        Impressum
-      </footer>
+      <footer className="footer">Impressum</footer>
     </Section>
   </div>
 );
---
+++ src/patterns/Intro/Intro.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Section from '../../components/Section';
+
+const Intro = Section.extend`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  background-color: rgb(245, 250, 255);
+  background: linear-gradient(90deg, transparent, rgba(0, 0, 0, .3) 70% );
+  color: #CCC;
+
+  height: 100vh;
+`;
+
+const IntroBlockquote = styled.blockquote`
+  font-size: var(--fontSize-40);
+  color: #CCC;
+
+  & footer {
+    margin-left: calc(1rem * var(--base-margin));
+    opacity: .5;
+    font-size: var(--fontSize-50);
+  }
+`;
+
+export default () => (
+  <Intro>
+    <IntroBlockquote>
+      <p>
+        „You see things; and you say 'Why?'<br />
+        But I dream things that never were; and I say 'Why not?'”
+      </p>
+      <footer>
+        —&nbsp;
+        <cite>
+          George Bernard Shaw
+        </cite>
+      </footer>
+    </IntroBlockquote>
+  </Intro>
+);
